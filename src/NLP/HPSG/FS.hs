0bd36e85b64c04dadc44de873bd535900838f1f0
Jakub Waszczuk <waszczuk.kuba@gmail.com>
2013-11-21 17:36:37 +0100

    Add feature structure data type

--- src/NLP/HPSG/FS.hs
+++ src/NLP/HPSG/FS.hs
@@ -3,36 +3,18 @@
 
 module NLP.HPSG.FS
 (
+-- * Feature structure
+  FS
+, FN (..)
+, FV
+-- * Unification
+, unifyWrong
 ) where
 
 
-import qualified Data.Set as S
+import           Control.Applicative ((<$>))
+import           Data.Traversable (traverse)
 import qualified Data.Map as M
-
-
--- -- IDEAS:
--- -- * We may also want to represent equality between FS substructures
--- --   by adding the (maybe) identifier to each feature value in the
--- --   `atts` map.
--- -- * We need to prepare a FS schema which described which feature
--- --   structures are valid and which are not.
--- 
--- 
--- -- | A feature structure parametrized over the type of the atomic
--- -- attributes and values.
--- data FS a = FS {
---     -- | Type of the feature structure.
---       ftyp  :: a
---     -- | Attributes and corresponding values.
---     , atts  :: M.Map a (FV a)
---     } deriving (Show, Eq, Ord)
--- 
--- 
--- -- | A feature value. 
--- data FV a
---     = Val a
---     | Set (S.Set a)
---     | Sub (FS a)
 
 
 -- On the basis of the description in the book:
@@ -51,54 +33,120 @@ import qualified Data.Map as M
 --
 -- 2#
 --
--- It can be concluded from 3.3.4, that it should be
--- possible to define FS templates.
+-- It can be concluded from 3.3.4, that it should be possible
+-- to define FS templates (abbreviated descriptions, in other words).
+-- Templates in 3.3.4 are simple, but perhaps it makes sense to allow
+-- functional (lambda) templates as well.
 
 
--- | Feature structure type.
-data FSType a = FSType {
-    -- | Name of the FS type.
-      fsType    :: a
-    -- | Map from attributes defined for a particular type to
-    -- values (names of other FS types or atomic entities).
-    -- Attribute names and value names can collide.
-    , fsAtts    :: M.Map a (FVType a)
+-- -- | Feature structure type.
+-- data FSType a = FSType {
+--     -- | Name of the FS type.
+--       fsType    :: a
+--     -- | Map from attributes defined for a particular type to
+--     -- values (names of other FS types or atomic entities).
+--     -- Attribute names and value names can collide.
+--     , fsAtts    :: M.Map a (FVType a)
+--     } deriving (Show, Eq, Ord)
+-- 
+-- 
+-- -- | A feature value.
+-- data FVType a
+--     = Ptr a           -- ^ Pointer to another feature type
+--     | Dom (S.Set a)   -- ^ Set of possible values
+-- 
+-- 
+-- ---------------------------------------------------------------------
+-- -- Hierarchy of types defined in the book
+-- ---------------------------------------------------------------------
+-- 
+-- 
+-- -- | FS hierarchy smart constructor.
+-- mkFS :: a -> [(a, a)] -> [FSType a] -> Tree (FSType a)
+-- mkFS x ys = Node (FSType x $ M.fromList ys)
+-- 
+-- 
+-- -- | Domain smart constructor.
+-- dom :: [a] -> FVType a
+-- dom xs = FVType . S.fromList
+-- 
+-- 
+-- -- | Hierarchy of FS types.
+-- typHier :: Tree (FSType String)
+-- typHier = mkFS "feat-struc" []
+--     [ mkFS "expression"
+--         [ ("head", Ptr "pos")
+--         , ("val", Ptr "val-cat") ]
+--         [ mkFS "phrase" []
+--         , mkFS "word" [] ]
+--     , mkFS "pos" []
+--         [ mkFS "agr-pos" [("agr", ?)]
+--             [ mkFS "noun" []
+--             , mkFS "verb" [("aux", ?)]
+--             , mkFS "det" [] ]
+--         , mkFS "prep"
+--         , mkFS "adj"
+--         , mkFS "conj" ]
+--     , mkFS "val-cat"
+--         [ ("comps", dom ["itr", "str", "dtr"])
+--         , ("spr", dom ["-", "+"]) ]
+
+
+---------------------------------------------------------------------
+-- Feature structures
+---------------------------------------------------------------------
+
+
+-- | A feature structure, possibly with variables.  Traditionally,
+-- the feature type is represented separately and always has an atomic
+-- value, but there seems to be no point in enforcing this on the level
+-- of the type system.
+type FS a = M.Map a (FN a)
+
+
+-- | A (posiblly named) feature value.
+data FN a = FN {
+    -- | A variable name (possibly)
+      varName   :: Maybe a
+--     -- | A set of potential values
+--     , valSet    :: S.Set (FV a)
+    -- | A feature value
+    , value     :: FV a
     } deriving (Show, Eq, Ord)
 
 
--- | An feature value.
-data FVType a
-    = Ptr a           -- ^ Pointer to another feature type
-    | Dom (S.Set a)   -- ^ Set of possible values
+-- | A feature value.
+data FV a
+    = Sub (FS a)        -- ^ Feature sub-structure
+    | Val a             -- ^ Atomic value
+    deriving (Show, Eq, Ord)
 
 
 ---------------------------------------------------------------------
--- Hierarchy of types defined in the book
+-- Unification
 ---------------------------------------------------------------------
 
 
--- | FS hierarchy smart constructor.
-mkFS :: a -> [(a, a)] -> [FSType a] -> Tree (FSType a)
-mkFS x ys = Node (FSType x $ M.fromList ys)
+-- | Disregard variables and perform simplified unification of the
+-- two given feature structures.
+unifyWrong :: Ord a => FS a -> FS a -> Maybe (FS a)
+unifyWrong s1 s2 = do
+    mut <- traverse id $ M.intersectionWith unifyFN s1 s2
+    return $ mut `M.union` s1 -|- s2
+  where
+    unifyFN (FN q x) (FN _ y) = FN q <$> unifyFV x y
+    unifyFV (Val x) (Val y) = if x == y
+        then Just $ Val x
+        else Nothing
+    unifyFV (Sub x) (Sub y) = Sub <$> unifyWrong x y
+    unifyFV _ _ = Nothing
 
 
--- | Domain smart constructor.
-mkDom :: [a] -> FVType a
-mkDom xs = FVType . S.fromList
+---------------------------------------------------------------------
+-- Misc
+---------------------------------------------------------------------
 
 
--- | Hierarchy of FS types.
-typHier :: Tree (FSType String)
-typHier = mkFS "feat-struc" []
-    [ mkFS "expression" [("head", Ptr "pos")]
-        [ mkFS "phrase" []
-        , mkFS "word" [] ]
-    , mkFS "pos" []
-        [ mkFS "agr-pos" [("agr", ?)]
-            [ mkFS "noun" []
-            , mkFS "verb" [("aux", ?)]
-            , mkFS "det" [] ]
-        , mkFS "prep"
-        , mkFS "adj"
-        , mkFS "conj" ]
-    , mkFS "val-cat" [("comps", mkDom ["itr", "str", "dtr"])]
+-- | Symmetric difference on maps.
+(-|-) :: Ord a => M.Map a b -> M.Map a b -> M.Map a b
+(-|-) x y = (x M.\\ y) `M.union` (y M.\\ x)
