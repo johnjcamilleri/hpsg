1444b8c5eda265513ea359c118391944a0c394e8
John J. Camilleri <john@johnjcamilleri.com>
2015-02-24 15:54:40 +0100

    Unification kindof works now (supporting variables!)
    Not extensively tested though

--- Main.hs
+++ Main.hs
@@ -6,28 +6,34 @@ import NLP.HPSG.AVM
 import qualified Data.Map as M
 import Data.Maybe
 
-_9  = mkAVM [(Attr "NUM",ValAtom "sing")]
-_10 = mkAVM [(Attr "PER",ValAtom "3rd")]
+_9  = mkAVM [("NUM",ValAtom "sing")]
+_10 = mkAVM [("PER",ValAtom "3rd")]
 
 _11 = _9 ⊔ _10
 
 _14 = _14a ⊔ _14b
 _14a = mkAVM [
-         (Attr "A",ValAVM (mkAVM [(Attr "B",ValAtom "a")]))
-       , (Attr "C",ValAVM (mkAVM [(Attr "D",ValAVM (mkAVM [(Attr "B",ValAtom "a")]))]))
+         ("A",ValAV (mkAV [("B",ValAtom "a")]))
+       , ("C",ValAV (mkAV [("D",ValAV (mkAV [("B",ValAtom "a")]))]))
        ]
 _14b = mkAVM [
-         (Attr "C",ValAVM (mkAVM [(Attr "D",ValAVM (mkAVM [(Attr "E",ValAtom "b")]))]))
+         ("C",ValAV (mkAV [("D",ValAV (mkAV [("E",ValAtom "b")]))]))
        ]
 
-_15 = _15a ⊔ _15b
+_15 = _15b ⊔ _15a
 _15a = (mkAVM [
-         (Attr "A",ValIndex 1)
-       , (Attr "C",ValAVM (mkAVM [(Attr "D",ValIndex 1)]))
-       ]) {avmDict = M.fromList [(1,AV (M.fromList [(Attr "B",ValAtom "a")]))]}
+         ("A",ValIndex 1)
+       , ("C",ValAV (mkAV [("D",ValIndex 1)]))
+       ]) {avmDict = M.fromList [(1,mkAV [("B",ValAtom "a")])]}
 _15b = mkAVM [
-         (Attr "C",ValAVM (mkAVM [(Attr "D",ValAVM (mkAVM [(Attr "E",ValAtom "b")]))]))
+         ("C",ValAV (mkAV [("D",ValAV (mkAV [("E",ValAtom "b")]))]))
        ]
 
-main = let Just avm = _14 in ppAVM avm
+main = do
+  ppAVM _15a
+  putStrLn ""
+  ppAVM _15b
+  putStrLn ""
+  let Just avm = _15
+  ppAVM avm
 -- main = ppAVM _14
--- NLP/HPSG/AVM.hs
+++ NLP/HPSG/AVM.hs
@@ -1,14 +1,18 @@
 module NLP.HPSG.AVM where
 
 import qualified Data.Map as M
+import qualified Control.Monad.State as CMS
 import Data.Maybe
+import Text.Printf (printf)
+
+import qualified Data.Traversable as Tr
 
 -- | Attribute or feature, often drawn in CAPS
 data Attribute = Attr String
   deriving (Eq, Ord, Show)
 
 -- | Value of an attribute
-data Value = ValAVM AVM      -- ^ A sub-structure
+data Value = ValAV AV        -- ^ A sub-structure
            | ValAtom Sort    -- ^ Atomic value
            | ValList [Value] -- ^ List of values
            | ValIndex Index  -- ^ Index to structure in dict
@@ -23,38 +27,54 @@ type Sort = String
 -- | An attribute-value matrix, with bound variables
 data AVM = AVM {
   -- ^ The inner AVM
-  avmBody :: M.Map Attribute Value,
+  avmBody :: AV,
   -- ^ Dictionary used for structure sharing
   avmDict :: M.Map Index AV
   }
   deriving (Eq, Ord, Show)
 
 -- | A map of attribute-values
-data AV = AV (M.Map Attribute Value)
-  deriving (Eq, Ord, Show)
+type AV = M.Map Attribute Value
+  -- deriving (Eq, Ord, Show)
+
+attrList :: [(String,v)] -> [(Attribute,v)]
+attrList = map (\(a,v)->(Attr a,v))
+
+attrMap :: [(String,v)] -> M.Map Attribute v
+attrMap = M.fromList . attrList
+
+-- | Make an AV
+mkAV :: [(String,Value)] -> AV
+mkAV = attrMap
 
 -- | Make an AVM with no name
-mkAVM :: [(Attribute,Value)] -> AVM
-mkAVM l = AVM (M.fromList l) M.empty
+mkAVM :: [(String,Value)] -> AVM
+mkAVM l = AVM (attrMap l) M.empty
 
--- | Make an AVM with a name
-mkAVMNamed :: Sort -> [(Attribute,Value)] -> AVM
-mkAVMNamed name l = AVM (M.fromList ((Attr "SORT",ValAtom name):l)) M.empty
+-- -- | Make an AVM with a name
+-- mkAVMNamed :: Sort -> [(Attribute,Value)] -> AVM
+-- mkAVMNamed name l = AVM (M.fromList ((Attr "SORT",ValAtom name):l)) M.empty
 
 ppAVM :: AVM -> IO ()
-ppAVM avm = go 0 avm
+ppAVM avm = do
+  go 0 (avmBody avm)
+  if M.size (avmDict avm) > 0
+  then do
+    putStrLn "where"
+    mapM_ (\(i,av) -> putStrLn (show i++": ") >> go 0 av) (M.toList $ avmDict avm)
+  else return ()
   where
-    go l (AVM b d) = do
-      case M.lookup (Attr "SORT") b of
+    go l av = do
+      case M.lookup (Attr "SORT") av of
         Just (ValAtom s) -> putStrLn $ (replicate l ' ') ++ ("[" ++ s)
         _ -> return ()
-      mapM_ (uncurry f) (M.toList b)
+      mapM_ (uncurry f) (M.toList av)
       where
         f (Attr a) v = do
           putStr (replicate l ' ')
           putStr $ "["++a++" "
           case v of
-            ValAVM avm -> putStr "\n" >> go (l+(length a)+1) avm
+            ValAV  av  -> putStr "\n" >> go (l+(length a)+1) av
             ValAtom s  -> putStrLn $ s
             ValList vs -> putStrLn $ "<"++replicate (length vs) '.'++">"
             ValIndex i -> putStrLn $ show i
@@ -64,20 +84,35 @@ ppAVM avm = go 0 avm
 
 -- | Unification
 unify :: AVM -> AVM -> Maybe AVM
-unify a1 a2 = Just $ AVM body dict
+unify a1 a2 = Just $ AVM body' dict'
   where
-    body = M.unionWith f (avmBody a1) (avmBody a2)
-    dict = M.union (avmDict a1) (avmDict a2)
-    f :: Value -> Value -> Value
-    f (ValAVM avm1) (ValAVM avm2) = ValAVM $ fromJust $ unify avm1 avm2
-    f (ValAtom a1) (ValAtom a2) | a1 == a2  = ValAtom a1
-                                | otherwise = error "Cannot unify"
-    f (ValList l1) (ValList l2) = ValList (l1++l2)
-    f (ValIndex i1) v2 = undefined
+    dict = M.unionWithKey (\k _ -> error $ "Conflicting key: "++show k) (avmDict a1) (avmDict a2)
+    (body',dict') = CMS.runState s (dict)
 
-    -- f (ValIndex i1) (ValIndex i2) | i1 == i2  = ValIndex i1
-    --                               | otherwise = error "Cannot unify"
-    f v1 v2 = error $ "Cannot unify:\n  "++show v1++"\n  "++show v2
+    s :: CMS.State (M.Map Index AV) AV
+    s = unionWithM f (avmBody a1) (avmBody a2)
+
+    f :: Value -> Value -> CMS.State (M.Map Index AV) Value
+    f (ValAV av1) (ValAV av2) = unionWithM f av1 av2 >>= \av -> return $ ValAV av
+    f (ValAtom a1) (ValAtom a2) | a1 == a2  = return $ ValAtom a1
+                                | otherwise = error $ printf "Cannot unify: %s and %s" a1 a2
+    f (ValList l1) (ValList l2) = return $ ValList (l1++l2)
+    f (ValIndex i1) v2 = do
+      v1 <- CMS.gets (\m -> m M.! i1)
+      ValAV v1' <- f (ValAV v1) v2 -- TODO
+      CMS.modify (\m -> M.insert i1 v1' m)
+      return $ ValIndex i1
+    f v1 (ValIndex i2) = do
+      v2 <- CMS.gets (\m -> m M.! i2)
+      ValAV v2' <- f v1 (ValAV v2) -- TODO
+      CMS.modify (\m -> M.insert i2 v2' m)
+      return $ ValIndex i2
+    f v1 v2 = error $ printf "Cannot unify:\n  %s\n  %s" (show v1) (show v2)
+
+-- See: http://stackoverflow.com/a/19896320/98600
+unionWithM :: (Monad m, Ord k) => (a -> a -> m a) -> M.Map k a -> M.Map k a -> m (M.Map k a)
+unionWithM f mapA mapB =
+  Tr.sequence $ M.unionWith (\a b -> do {x <- a; y <- b; f x y}) (M.map return mapA) (M.map return mapB)
 
 -- | AVMs may subsume eachother
 subsumes :: AVM -> AVM -> Ordering
